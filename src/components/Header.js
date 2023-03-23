02b3630406c0b8b5e8d0c60ce7cc5b1f950d622f
Lauren Bentley <laurendebentley@gmail.com>
2023-03-23 17:00:46 -0400

    header started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+export default function Greeting() {
+    // To set a state variable using `useState`, we give our variable a name of `greeting` and a function to update it.
+    // We also provide an initial value
+    
+  
+    return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Lauren Bentley</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+      <ul class="col-10 navbar-nav d-flex justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    );
+  }
