9f2ab9f686f36b9b3f83ec12dcf8ea197167f746
Lauren Bentley <laurendebentley@gmail.com>
2023-03-23 16:22:46 -0400

    react imports app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Landing from './components/Landing'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
 
 function App() {
   return (
