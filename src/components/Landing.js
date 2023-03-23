ba8fc03910d4340f014bb76bdeaaa9dd7dc523d4
Lauren Bentley <laurendebentley@gmail.com>
2023-03-23 18:41:03 -0400

    viewport fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import github from '../assets/github.svg'
+import linkedin from '../assets/linkedin.svg'
 
 export default function Landing() {
 
     return (
-        <div className="jumbotron jumbotron-fluid">
+        <div className="landing jumbotron jumbotron-fluid">
   <div className="container">
-    <h1 className="display-4">Fluid jumbotron</h1>
-    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <h1 className="display-4">Hi, I'm Lauren Bentley.</h1>
+    <h3 className="display-9">I'm a fullstack developer.</h3>
+    <p className='display-9'>Check me out.</p>
+  </div>
+  <div className='container landing-links'>
+    <img className= "landing-btn" src={github}></img>
+    <img className= "landing-btn" src={linkedin}></img>
   </div>
 </div>
     )
