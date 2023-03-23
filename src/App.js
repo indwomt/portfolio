dfef5a8225f5b9a4de29831784c8865cde64698f
Lauren Bentley <laurendebentley@gmail.com>
2023-03-23 14:12:03 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editssssss <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +17,10 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      
+      
     </div>
   );
 }
