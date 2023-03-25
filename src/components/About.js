7c92a657f9dca2720dec5dc5ac7f81a81cb3b632
Lauren Bentley <laurendebentley@gmail.com>
2023-03-25 14:24:57 -0400

    headshot added to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import headshot from '../assets/headshot.jpg'
 
 export default function About() {
     return (
         <div className="container about-parent">
             <div className="row about-content align-text-center">
-                <div className="about-img col-6 align-text-center align-items-center d-flex justify-content-center">
-                    <h3>Test</h3>
+                <h2 className='about-header'>About</h2>
+                
+                <div className="about-img col-md-6 col-sm-12 align-text-center align-items-center d-flex justify-content-center">
+                    <img className='headshot'src={headshot}></img>
                 </div>
-                <div className="col-6">
-                    <span className="about-text"> A web developer with a growth-mindset with a passion for collaboration and problem-solving.</span>
+                <div className="col-md-6 col-sm-12">
+                    <span className="about-text"> A web developer with a growth-mindset and a passion for collaboration and problem-solving.</span>
                     <br></br>
                     <br></br>
                     <span className=" about-text">Currently, I am a business manager at <a alt='I made this site!'href="#">Westchester Wags Co</a> where I oversee administration and technical solutions with a focus on increasing web presence and scalability.</span>
