b2f32fd1d3870049e92a0eab330c94a8d54d54c6
Lauren Bentley <laurendebentley@gmail.com>
2023-04-08 11:46:58 -0400

    headshot fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Cards from '../components/cards'
 
+import oopScreenshot from '../assets/OOP-screenshot.PNG';
+import dayInLifeScreenshot from '../assets/day-in-life-screenshot.PNG';
+import marsScreenshot from '../assets/mars-screenshot.PNG';
+import readmeGeneratorScreenshot from '../assets/readme-generator.PNG';
 
 
 
@@ -8,7 +12,7 @@ const projects = [
     {
         id: 0,
         title: "Team Generator || OOP",
-        image: '/assets/OOP-screenshot.PNG',
+        image: {oopScreenshot},
         description: "Allows users to create employee profiles through Node.js, Inquirer package, and OOP concepts.",
         repo:"github.com/indwomt/OOP-team-generator"
     },
@@ -16,20 +20,20 @@ const projects = [
     {
         id: 1,
         title: 'Day in the Life',
-        image:"/assets/day-in-life-screenshot.PNG",
+        image:{dayInLifeScreenshot},
         description:'A full stack application that uses Express, Node, MySQL, and Handlebars. Users can create an account, make posts, and comment on other posts made by other users'
     },
 
     {
         id: 2,
-        image:'/assets/mars-screenshot.PNG',
+        image: {marsScreenshot},
         title: "Coding the Cosmos",
         description: "Users can look up mars rover photos from "
     },
 
     {
         id: 3,
-        image: '/assets/readme-generator.PNG',
+        image: '../assets/readme-generator.PNG',
         title: "README.md Generator"
     }
 ]
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import resume from '../assets/BENTLEYResume.pdf'
+import resume from '../assets/BENTLEYResume.pdf'
 
 
 export default function Resume() {
     return (
         <div className="container">
             <div className='resume container d-flex-col align-items-center justify-content-center col-12'>
-            {/* <h2 className="resume-header align-self-start">Resume</h2> */}
+            <h2 className="resume-header align-self-start">Resume</h2>
             
             <h3>Want to know more about my career?</h3>
-            {/* <a href={resume}>Resume</a> */}
+            <a href={resume}>Resume</a>
             </div>
 
         </div>
